ae69ecc0510005f3d5c005fcfc966e8e33f1f80c
Lukas Marx <marxlukas@googlemail.com>
2018-02-17 10:54:10 +0100

    testing correct depoyment

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,5 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_2f8bf399dc0c_key -iv $encrypted_2f8bf399dc0c_iv -in deploy-key.enc -out deploy-key -d
-rm deploy-key.enc # Don't need it anymore
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+chmod 600 do-deployPPK
+mv do-deployPPK ~/.ssh/id_rsa
