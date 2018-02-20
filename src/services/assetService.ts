63cc95df96be5a3857e24e9f14e581c2ea045c14
Lukas Marx <marxlukas@googlemail.com>
2018-02-20 08:09:56 +0100

    re-added graphql image capabilities

--- src/services/assetService.ts
+++ src/services/assetService.ts
@@ -8,62 +8,26 @@ import { Collection } from 'mongodb';
 const s3 = new S3();
 
 export class AssetService {
-  async getAll(projectId: string) {
-    const db = await database.connect();
-    const assets = <Collection<Asset>>db.collection('assets');
+    async getAll(projectId: string) {
+        const db = await database.connect();
+        const assets = <Collection<Asset>>db.collection('assets.files');
 
-    const result = await assets.find({ projectId: projectId }).toArray();
+        const result = await assets
+            .find({ metadata: { projectId: projectId } })
+            .toArray();
 
-    return result;
-  }
-
-  async insert(projectId: string, event: any) {
-    const boundary = mp.getBoundary(event.headers['content-type']);
-    const bodyBuffer = new Buffer(event.body.toString(), 'base64');
-    const files = mp.Parse(bodyBuffer, boundary);
-    for (let key in files) {
-      let file = files[key];
-
-      const s3Parmas = {
-        Bucket: 'grapqlcms',
-        Key: 'images/' + projectId + file.filename,
-        Body: file.data,
-        ContentType: file.type
-      };
-
-      await s3.putObject(s3Parmas).promise();
-
-      const asset = new Asset();
-      asset.fileName = file.filename;
-      asset.projectId = projectId;
-      asset.type = file.type;
-      asset.size = file.data.length;
-
-      const db = await database.connect();
-      const assets = <Collection<Asset>>db.collection('assets');
-
-      await assets.insertOne(asset);
-      return asset;
+        return result;
     }
-  }
 
-  async get(projectId: string, id: string): Promise<Asset> {
-    const db = await database.connect();
-    const assets = <Collection<Asset>>db.collection('assets');
+    async get(projectId: string, id: string): Promise<Asset> {
+        const db = await database.connect();
+        const assets = <Collection<Asset>>db.collection('assets.files');
 
-    return assets.findOne({ projectId: projectId, fileName: id });
-  }
-
-  async getBlob(projectId: string, name: string, width?: number, format?: string): Promise<any> {
-    let key = 'images/' + projectId + name;
-
-    const s3Parmas: any = {
-      Bucket: 'grapqlcms',
-      Key: key
-    };
-    const firstTry = await s3.getObject(s3Parmas).promise();
-    return firstTry;
-  }
+        return assets.findOne({
+            metadata: { projectId: projectId },
+            filename: id
+        });
+    }
 }
 
 export const assetService = new AssetService();
