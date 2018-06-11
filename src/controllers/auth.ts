962aa5226e47127a74955c76c1a6f40d7861ff1f
Lukas Marx <marxlukas@googlemail.com>
2018-06-11 12:01:30 +0200

    added try catch to adminToken to diagnose

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -7,24 +7,28 @@ import * as jwt from 'jsonwebtoken';
 
 export const adminToken = async (req: Request, res: Response) => {
     const body = req.body;
+    try {
+        if (!body.email || !body.password) return res.sendStatus(400);
 
-    if (!body.email || !body.password) return res.sendStatus(400);
+        const db = await database.connect();
+        const values = <Collection<Account>>db.collection('accounts');
+        const account = await values.findOne({ email: body.email });
+        if (!account) return res.sendStatus(400);
 
-    const db = await database.connect();
-    const values = <Collection<Account>>db.collection('accounts');
-    const account = await values.findOne({ email: body.email });
-    if (!account) return res.sendStatus(400);
+        if (await bcrypt.compare(body.password, account.password)) {
+            const token = {};
 
-    if (await bcrypt.compare(body.password, account.password)) {
-        const token = {};
-
-        const signedToken = jwt.sign(token, process.env['JWT_SECRET'], {
-            audience: process.env.TOKEN_AUDIENCE,
-            issuer: process.env.TOKEN_ISSUER,
-            subject: body.email,
-            expiresIn: '24h'
-        });
-        return res.json({ token: signedToken });
+            const signedToken = jwt.sign(token, process.env['JWT_SECRET'], {
+                audience: process.env.TOKEN_AUDIENCE,
+                issuer: process.env.TOKEN_ISSUER,
+                subject: body.email,
+                expiresIn: '24h'
+            });
+            return res.json({ token: signedToken });
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        return res.sendStatus(400);
     }
-    return res.sendStatus(400);
 };
